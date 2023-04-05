05be9273f9af776af66f5e1c92a722ae5f2bfa64
Rahmat-Afriyadi <76650875+Rahmat-Afriyadi@users.noreply.github.com>
2023-04-05 22:04:41 +0700

    Update archived.controller.ts
    Refac: Clean Console.log

--- src/modules/machines/controllers/archived.controller.ts
+++ src/modules/machines/controllers/archived.controller.ts
@@ -16,13 +16,11 @@ export const archived = async (req: Request, res: Response, next: NextFunction) 
 
     const archivedMachineService = new ArchivedMachineService(db);
     const result = await archivedMachineService.handle(req.params.id, session);
-    console.log(" ini service ", result);
 
     await db.commitTransaction();
 
     res.status(204).json({ result });
   } catch (error) {
-    console.log("ini catch", error);
     await db.abortTransaction();
     next(error);
   } finally {
